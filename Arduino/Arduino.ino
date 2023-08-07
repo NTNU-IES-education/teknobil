ee21dbb16ee97ca960d84663b278adb95d29ad82
PeterhdPham <92777892+PeterhdPham@users.noreply.github.com>
2023-08-07 10:56:28 +0200

    Update Arduino.ino

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,3 +1,4 @@
+
 //MOTOR SETUP
 const int E1 = 3; ///<Motor1 Speed - Front Right
 const int E2 = 11;///<Motor2 Speed - Front Left
@@ -24,8 +25,8 @@ int right = 0;
 
 
 //SPEEDS
-int drivingSpeed = 150;
-int turningSpeed = 50;
+int drivingSpeed = 250;
+int turningSpeed = 150;
 
 enum Motor {
   MotorRight,
